fcd5db584c8effa58a45039013157a182f391ab9
Kabir Tawhid <kabirfromtawhid@gmail.com>
2023-08-10 00:43:25 +0600

    Styled form Heading, added color to theme, styled input element and it's placeholder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*{.html,.js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      montserrat: ["Montserrat"],
+    },
+
+    extend: {
+      colors: {
+        "dark-1": "#4E5150",
+        "gray-1": "#333333",
+        "gray-2": "#4F4F4F",
+        "gray-3": "#828282",
+        orange: "#F2994A",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
